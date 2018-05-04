717f26e46df5a0c4a97232f9caf76124950f77ed
PhucAn <38677949+phucanvn2012@users.noreply.github.com>
2018-05-04 20:21:57 +0700

    to Nhan

---
+++ index.js
@@ -0,0 +1,48 @@
+// for return res to client
+var express = require("express");
+var app = express();
+app.use(express.static("public")); // all req sent to PUBLIC folder
+
+// Use to render page
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Create server
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(80); // port
+
+// Connect DB
+/*var mongoose = require("mongoose"); // Add MONGOOSE module
+mongoose.connect("mongodb://localhost/local")
+*/
+
+// Listen for connection, when connection -> call this
+io.on("connection", function(socket){
+	console.log("have connection");
+});
+// Render page
+app.get("/", function(req, res){
+	res.render("index");
+});
+console.log("Serer is running");
+
+// test DB connection
+// create table
+/*var Test = mongoose.model("User", {name: String, ID: Number, title: String});
+// create row
+var user1 = new Test({name: "Lorem", ID: 1, title: "Ipsum"});
+
+console.log(user1);
+
+// save to DB
+user1.save(function(err, userObj) {
+	if(err) {
+		console.log(err);
+	}
+	else {
+		console.log("successful");
+	}
+});*/
+
+
