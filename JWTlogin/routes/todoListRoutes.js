55eeab513503c015546c058cb9bab03a5fb70eb0
Quoc Trung <cs2015_trung.pq@student.vgu.edu.vn>
2018-05-07 09:25:34 +0700

    Security for web app

---
+++ JWTlogin/routes/todoListRoutes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(app) {
+   	var userHandlers = require('../controllers/userController.js');
+
+   	app.route('/auth/register').post(userHandlers.register);
+
+   	app.route('/auth/sign_in').post(userHandlers.sign_in);
+};
