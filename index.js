add063d1c1527efe247f2389906bad9ec78fd229
PhucAn <38677949+phucanvn2012@users.noreply.github.com>
2018-05-08 12:58:05 +0700

    Full

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 // for return res to client
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
+mongoose = require('mongoose'),
+  Task = require('./models/todoListModel'),
+  User = require('./models/userModel'),
+  bodyParser = require('body-parser'),
+  jsonwebtoken = require("jsonwebtoken");
 app.use(express.static("public")); // all req sent to PUBLIC folder
-
+//app.use(bodyParser.urlencoded({ extended: true })); 
+mongoose.connect('mongodb://localhost/Tododb', { useMongoClient: true });
 // Use to render page
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+      if (err) req.user = undefined;
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+
+var routes = require('./routes/todoListRoutes');
+routes(app);
 // Create server
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
@@ -25,7 +51,8 @@ io.on("connection", function(socket){
 app.get("/", function(req, res){
 	res.render("index");
 });
-console.log("Serer is running");
+
+console.log("Server is running");
 
 // test DB connection
 // create table
