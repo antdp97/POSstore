71af4cd0444a6ce974767d5068d9523f81685712
PhucAn <38677949+phucanvn2012@users.noreply.github.com>
2018-05-09 22:20:10 +0700

    Delete todoListRoutes.js

--- JWTlogin/routes/todoListRoutes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-   	var userHandlers = require('../controllers/userController.js');
-
-   	app.route('/auth/register').post(userHandlers.register);
-
-   	app.route('/auth/sign_in').post(userHandlers.sign_in);
-};
