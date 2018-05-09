8d62c8397aabc1fd871ed939892e5d9013e9e6b7
nhan1603 <38678910+nhan1603@users.noreply.github.com>
2018-05-09 22:30:07 +0700

    Add files via upload

---
+++ demo/controllers/userController.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  jwt = require('jsonwebtoken'),
+  bcrypt = require('bcrypt'),
+  User = mongoose.model('users');
+
+exports.register = function(req, res) {
+  var newUser = new User(req.body);
+  newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
+  newUser.save(function(err, user) {
+    if (err) {
+      return res.status(400).send({
+        message: err
+      });
+    } else {
+      user.hash_password = undefined;
+      return res.json(user);
+    }
+  });
+};
+
+exports.sign_in = function(req, res) {
+  console.log(req.body.Email);
+  User.findOne({
+    email: req.body.Email
+  }, function(err, user) {
+    if (err) throw err;
+   if (!user){
+      return res.status(401).json({ message: 'Authentication failed. Invalid username.' });
+    }
+    else if (!user.comparePassword(req.body.password)) {
+      return res.status(401).json({ message: 'Authentication failed. Invalid password.' });
+    }
+    return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs') });
+  });
+};
+
+exports.loginRequired = function(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    return res.status(401).json({ message: 'Unauthorized user!' });
+  }
+};
